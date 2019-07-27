5b31519ac8cb54b0f0a3081d7b550deb130602c9
Miguel Campos <amodestduck@gmail.com>
2019-07-27 11:06:52 -0700

    Add room system
    -
    -
    -

--- server.js
+++ server.js
@@ -20,18 +20,48 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/html/index.html');
 });
 
+app.get('/lobby/:lobbyID/user/:userName', function (req, res) {
+  res.send(req.params)
+});
 
-let statusManager = io.of('/player-status');
+let _users = new Map();
+let _rooms = new Map();
 //socket management
-statusManager.on('connection', function (socket) {
+io.on('connection', function (socket) {
   console.log(`Socket ${socket.id} has connected`);
-  statusManager.emit('update users', socket.id);
+  _users.set(socket.id, socket.id);
+  io.emit('update users', Array.from(_users.values()));
 
   socket.on('disconnect', function () {
     console.log(`A user has disconnected: ${socket.id}.`);
-    io.emit('update users', socket.id);
+    _users.delete(socket.id);
+    io.emit('update users', Array.from(_users.values()));
+  });
+
+  socket.on('new room', function (roomName) {
+    if (!_rooms.has(roomName)) {
+      io.emit('update rooms', 'test');
+      _rooms.set(roomName, {
+        users: [],
+        capacity: 4,
+        inProgress: false,
+        socket: io.to(roomName)
+      });
+      console.log(`A new room ${roomName} has been created!`);
+    }
+    socket.emit('join room', roomName);
+  });
+
+  socket.on('join room', function (roomName) {
+    let room = _rooms.get(roomName);
+    if (room.users.length < 4 && !room.users.includes(socket.id)) {
+      socket.join(roomName);
+      room.users.push(socket.id);
+      io.to(roomName).emit('new user', 'test');
+      console.log(`User: ${_users.get(socket.id)} has successfully joined the room ${roomName}.`);
+    } else {
+      socket.emit('join fail', roomName);
+      console.log(`User: ${_users.get(socket.id)} has failed to join the room ${roomName}.`);
+    }
   });
 });
-
-
-
